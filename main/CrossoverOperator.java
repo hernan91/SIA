dfec531e6815f9a868460e34c5e5a30498eff583
Hernan da Silva <hernanunpa@Gmail.com>
2018-05-09 12:01:17 -0300

    09/05

--- main/CrossoverOperator.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-import java.util.Iterator;
-
-public abstract class CrossoverOperator {
-	public abstract Iterator<Individual> crossover(Individual ind1, Individual ind2);
-}
--- main/ObjectiveFunction.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-public abstract class ObjectiveFunction {
-	public abstract double obtainFitness(Individual individual);
-	
-}
