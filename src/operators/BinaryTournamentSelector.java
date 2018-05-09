9d389f12c0ba7fda8a2b307b366a804a1aa0d867
Hernan da Silva <hernanunpa@Gmail.com>
2018-05-09 12:14:47 -0300

    09/05

--- src/operators/BinaryTournamentSelector.java
+++ src/operators/BinaryTournamentSelector.java
@@ -1,6 +1,7 @@
 package operators;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 import generics.Individual;
@@ -8,29 +9,35 @@ import generics.ObjectiveFunction;
 import generics.Population;
 
 //Selection strategy
-public class BinaryTournamentSelector{
+public class BinaryTournamentSelector extends Operator{
 	private ObjectiveFunction objFunction;
 	
 	public BinaryTournamentSelector(ObjectiveFunction objFunction) {
 		this.objFunction = objFunction;
 	}
 	
-	private Individual doTournamentRound(Population population) {
-		ArrayList<Individual> individuals =  population.getIndividuals();
+	private Individual doTournamentRound(ArrayList<Individual> population) {
+		ArrayList<Individual> individuals =  population;
 		Random rand = new Random();
-		int randNum1 = rand.nextInt(population.getNumberOfIndividuals()-1);
-		int randNum2 = rand.nextInt(population.getNumberOfIndividuals()-1);
+		int randNum1 = rand.nextInt(population.size()-1);
+		int randNum2 = rand.nextInt(population.size()-1);
 		Individual individual1 = individuals.get(randNum1).clone();
 		Individual individual2 = individuals.get(randNum2).clone();
 		if(individual1.compareTo(individual2, objFunction)==1) return individual1;
 		return individual2;
 	}
 	
-	public Population selectIndividuals(Population population) {
+	public ArrayList<Individual> selectIndividuals(ArrayList<Individual> population) {
 		ArrayList<Individual> selectedIndividuals =  new ArrayList<Individual>();
-		for(int i=0; i<population.getNumberOfIndividuals(); i++) {
+		for(int i=0; i<population.size(); i++) {
 			selectedIndividuals.add(doTournamentRound(population));
 		}
 		return new Population(selectedIndividuals);
+	}
+
+	@Override
+	public Iterator<Individual> execute(Population population) {
+		selectIndividuals(Population)
+		return null;
 	}
 }
