8c42b4134133330036fbf86652f84b5cb2ba4c6c
Hernan da Silva <hernanunpa@Gmail.com>
2018-08-01 19:34:38 -0300

    01/08

--- r/DrawCoverageGridTestSquare.r
+++
@@ -1,8 +0,0 @@
-coveredPoints <- read.csv("~/git/SIA/squareGridSolution.csv", header = TRUE, sep = ",", dec = ".")
-plot(coveredPoints, col = 'black')
-
-centerX = 30
-centerY = 30
-range=10
-
-rect(centerX-range, centerY-range, centerX+range-1, centerY+range-1, col=rgb(1,0,0,alpha =0.6),lty=1,lwd=0.3)
--- r/grafiacadorSensoresRadioCircular.R
+++
@@ -1,2 +0,0 @@
-grfica
-grfica
